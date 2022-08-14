ad6e9f622b4d601929b7c993093f75c6036907cf
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2022-08-14 16:15:00 +0200

    Move TokenType to the token.go source file. Add String method

--- lox.go
+++ lox.go
@@ -10,56 +10,6 @@ type Lox struct {
 	hadError bool
 }
 
-const (
-	// Single-character tokens.
-	LeftParen TokenType = iota + 1
-	RightParen
-	LeftBrace
-	RightBrace
-	Comma
-	Dot
-	Minus
-	Plus
-	Semicolon
-	Slash
-	Star
-
-	// One or two character tokens
-	Bang
-	BangEqual
-	Equal
-	EqualEqual
-	Greater
-	GreaterEqual
-	Less
-	LessEqual
-
-	// Literals
-	Identifier
-	String
-	Number
-
-	// Keywords
-	And
-	Class
-	Else
-	False
-	Fun
-	For
-	If
-	Nil
-	Or
-	Print
-	Return
-	Super
-	This
-	True
-	Var
-	While
-
-	Eof
-)
-
 func runFile(filePath string) {
 	fileBytes, err := os.ReadFile(filePath)
 	if err != nil {
--- token.go
+++ token.go
@@ -8,10 +8,153 @@ type Object struct {
 	data any
 }
 
+func (o *Object) String() string {
+	return fmt.Sprintf("%s", o.data)
+}
+
 type TokenType int
 
+const NumTokens = 39
+
+// Enumeration describing all the different token types.
+// Importatin invariant to uphold is that the first token
+// should have enum value equal to 1, e.g. iota + 1,
+// and the Eof token should always be the last. This gives
+// and easy way of asserting that the number of possible tokens
+// matches that which are specified in the NumTokens constant.
+const (
+	// Single-character tokens.
+	LeftParen TokenType = iota + 1
+	RightParen
+	LeftBrace
+	RightBrace
+	Comma
+	Dot
+	Minus
+	Plus
+	Semicolon
+	Slash
+	Star
+
+	// One or two character tokens
+	Bang
+	BangEqual
+	Equal
+	EqualEqual
+	Greater
+	GreaterEqual
+	Less
+	LessEqual
+
+	// Literals
+	Identifier
+	String
+	Number
+
+	// Keywords
+	And
+	Class
+	Else
+	False
+	Fun
+	For
+	If
+	Nil
+	Or
+	Print
+	Return
+	Super
+	This
+	True
+	Var
+	While
+
+	Eof
+)
+
 func (t TokenType) String() string {
-	return fmt.Sprintf("")
+	switch t {
+	case LeftParen:
+		return "LeftParen"
+	case RightParen:
+		return "RightParen"
+	case LeftBrace:
+		return "LeftBrace"
+	case RightBrace:
+		return "RightBrace"
+	case Comma:
+		return "Comma"
+	case Dot:
+		return "Dot"
+	case Minus:
+		return "Minus"
+	case Plus:
+		return "Plus"
+	case Semicolon:
+		return "Semicolon"
+	case Slash:
+		return "Slash"
+	case Star:
+		return "Star"
+	case Bang:
+		return "Bang"
+	case BangEqual:
+		return "BangEqual"
+	case Equal:
+		return "Equal"
+	case EqualEqual:
+		return "EqualEqual"
+	case Greater:
+		return "Greater"
+	case GreaterEqual:
+		return "GreaterEqual"
+	case Less:
+		return "Less"
+	case LessEqual:
+		return "LessEqual"
+	case Identifier:
+		return "Identifier"
+	case String:
+		return "String"
+	case Number:
+		return "Number"
+	case And:
+		return "And"
+	case Class:
+		return "Class"
+	case Else:
+		return "Else"
+	case False:
+		return "False"
+	case Fun:
+		return "Fun"
+	case For:
+		return "For"
+	case If:
+		return "If"
+	case Nil:
+		return "Nil"
+	case Or:
+		return "Or"
+	case Print:
+		return "Print"
+	case Return:
+		return "Return"
+	case Super:
+		return "Super"
+	case This:
+		return "This"
+	case True:
+		return "True"
+	case Var:
+		return "Var"
+	case While:
+		return "While"
+	case Eof:
+		return "Eof"
+	default:
+		return fmt.Sprintf("%d", int(t))
+	}
 }
 
 type Token struct {
@@ -26,7 +169,5 @@ func NewToken(tokenType TokenType, lexeme string, literal Object, line int) Toke
 }
 
 func (t *Token) String() string {
-	// TODO: Do proper string formatting
-	// return fmt.Sprintf("%b", t)
-	return ""
+	return fmt.Sprintf("%s %s %s", t.tokenType, t.lexeme, t.literal)
 }
